d6beb93dfa53983946d5a9f1702792d1b6c4d46c
Attila Bagossy <bagossyattila@outlook.com>
2026-04-30 18:31:00 +0200

    feat: configured LikeC4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,43 +3,50 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 import react from "@astrojs/react";
+import { LikeC4VitePlugin } from "likec4/vite-plugin";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "My Docs",
-    social: [
-      {
-        icon: "github",
-        label: "GitHub",
-        href: "https://github.com/withastro/starlight",
-      },
-    ],
-    sidebar: [
-      {
-        label: "Esettanulmány",
-        slug: "esettanulmany",
-      },
-      {
-        label: "SRS",
-        link: "/srs.doc",
-      },
-      {
-        label: "Architekturális karakterisztikák",
-        slug: "ac",
-      },
-      {
-        label: "Architekturálisan szignifikáns követelmények",
-        slug: "asr",
-      },
-      {
-        label: "Architekturális stílus",
-        slug: "as",
-      },
-      {
-        label: "Döntési jegyzőkönyvek",
-        autogenerate: { directory: "adr" },
-      },
-    ],
-  }), react()],
+  vite: {
+    plugins: [LikeC4VitePlugin({})],
+  },
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Esettanulmány",
+          slug: "esettanulmany",
+        },
+        {
+          label: "SRS",
+          link: "/srs.doc",
+        },
+        {
+          label: "Architekturális karakterisztikák",
+          slug: "ac",
+        },
+        {
+          label: "Architekturálisan szignifikáns követelmények",
+          slug: "asr",
+        },
+        {
+          label: "Architekturális stílus",
+          slug: "as",
+        },
+        {
+          label: "Döntési jegyzőkönyvek",
+          autogenerate: { directory: "adr" },
+        },
+      ],
+    }),
+    react(),
+  ],
 });
