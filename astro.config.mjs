7db9d58f9a3ecb4ce4a69ef30a11b8a1a6610222
Attila Bagossy <bagossyattila@outlook.com>
2026-04-30 18:11:42 +0200

    feat: installed react and likec4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,44 +2,44 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "My Docs",
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/withastro/starlight",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Esettanulmány",
-          slug: "esettanulmany",
-        },
-        {
-          label: "SRS",
-          link: "/srs.doc",
-        },
-        {
-          label: "Architekturális karakterisztikák",
-          slug: "ac",
-        },
-        {
-          label: "Architekturálisan szignifikáns követelmények",
-          slug: "asr",
-        },
-        {
-          label: "Architekturális stílus",
-          slug: "as",
-        },
-        {
-          label: "Döntési jegyzőkönyvek",
-          autogenerate: { directory: "adr" },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: "My Docs",
+    social: [
+      {
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/withastro/starlight",
+      },
+    ],
+    sidebar: [
+      {
+        label: "Esettanulmány",
+        slug: "esettanulmany",
+      },
+      {
+        label: "SRS",
+        link: "/srs.doc",
+      },
+      {
+        label: "Architekturális karakterisztikák",
+        slug: "ac",
+      },
+      {
+        label: "Architekturálisan szignifikáns követelmények",
+        slug: "asr",
+      },
+      {
+        label: "Architekturális stílus",
+        slug: "as",
+      },
+      {
+        label: "Döntési jegyzőkönyvek",
+        autogenerate: { directory: "adr" },
+      },
+    ],
+  }), react()],
 });
