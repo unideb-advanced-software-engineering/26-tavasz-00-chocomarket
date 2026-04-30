8916bba4f516cd2df7f3f29e3309e650a931e8e4
Attila Bagossy <bagossyattila@outlook.com>
2026-04-30 18:05:33 +0200

    feat: sidebar config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Esettanulmány",
+          slug: "esettanulmany",
+        },
+        {
+          label: "SRS",
+          link: "/srs.doc",
+        },
+        {
+          label: "Architekturális karakterisztikák",
+          slug: "ac",
+        },
+        {
+          label: "Architekturálisan szignifikáns követelmények",
+          slug: "asr",
+        },
+        {
+          label: "Architekturális stílus",
+          slug: "as",
+        },
+        {
+          label: "Döntési jegyzőkönyvek",
+          autogenerate: { directory: "adr" },
+        },
+      ],
+    }),
+  ],
 });
